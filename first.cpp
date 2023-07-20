422224ef635624f72960716d126db472b7d5fafe
iSHaN646 <131184639+iSHaN646@users.noreply.github.com>
2023-07-20 06:05:33 +0000

    first file created

---
+++ first.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    cout<<"hello"<<endl;
+    int n;
+    cout<<"Enter n: ";
+    cin>>n;
+    cout<<"the no. is: "<<n*2<<endl;
+
+
+
+    return 0;
+}
